cbb7b41b985d01e863cc4a65f42c8204f5e60c50
walnutown <walnutown@gmail.com>
2013-06-03 15:23:21 -0700

    install Crashlytics

--- src/com/example/liv/MainActivity.java
+++ src/com/example/liv/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.liv;
 
+import com.crashlytics.android.Crashlytics;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,6 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Crashlytics.start(this);
 		setContentView(R.layout.activity_main);
 	}
 
